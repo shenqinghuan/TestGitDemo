5349cd9c7ea0d13d5f2ab417ec66557f7d796fee
penghuan <penghuan@weeget.cn>
2020-12-28 18:50:16 +0800

    提交修改代码

--- add/GetName.java
+++ add/GetName.java
@@ -1,5 +1,5 @@
 public class CustomErrorHandler{
     public static void main(String[] args){
-		System.out.println("-----------------------输出名字");
+		System.out.println("-----------------------输");
 	}
 }
